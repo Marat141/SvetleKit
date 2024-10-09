3337671d8a6d82621001ca4b2476ff50126d93aa
Marat141 <terest141@gmail.com>
2024-10-09 16:22:06 +0200

    Konecne-2

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<header>
+    <nav>
+        <a href="/">Home page</a>
+        <a href="/">About</a>
+        <a href="/">Svelte</a>   
+    </nav>
+
+</header>
+
+<style>
+    header{
+        width: 100%;
+        height: 100px;
+        padding: 36px;
+        box-sizing: border-box;
+        background-color: rgba(61, 61, 62, 0.607);
+        
+    }
+
+    nav{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        gap: 36px;
+        justify-content: flex-end;
+        align-items: center; 
+    }
+    a{
+            color: white;
+            font-weight: 100%;
+            text-decoration: none;
+            transition: ease .3s;
+
+
+    }
+    a:hover{
+        color: red;
+    }
+
+
+</style>
--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<a href="/">Home</a>
+<script>
+
+</script>
+
+
+<section class="hero">
+    <img src="https://www.souepl.cz/wp-content/uploads/2016/10/logo_stredni-1.jpg" alt="">
+    <h1>Střední odborné učiliště, elektrotechnické Plzeň</h1>
+    <p>Pojďte stavět svaší budoucnost u nás na škole, kde propojujeme praxi a vzdělání</p>
+    <div class="hero-buttons">
+        <a href="/about">Login</a>
+        <a href="/" id="ghost">About</a>
+    </div>
+</section>
+
+<style>
+    .hero{
+        width: 100%;
+        min-height: calc(100vh - 100px);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+    }
+
+    .hero img{
+        width: 250px;
+    }
+
+    .hero-buttons{
+        display: flex;
+        gap: 36px;
+
+    }
+
+    .hero-buttons a{
+        color: white;
+        background-color: rgb(36, 118, 29);
+        text-decoration: none;
+        width: 150px;
+        height: 50px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        font-weight: 500;
+        font-family: Arial, Helvetica, sans-serif;
+        transition: ease 0.5s;
+    }
+
+
+    #ghost {
+        color: black;
+        background: none;
+        border: 1px solid black;
+        transition: ease 0.3s;
+    }
+
+    #ghost:hover{
+        color: white;
+        background: red; 
+        border: 1px solid rgb(0, 0, 0); 
+    }
+
+    .hero-buttons a:hover{
+        color: rgb(0, 0, 0);
+        background: red;
+    }
+</style>
